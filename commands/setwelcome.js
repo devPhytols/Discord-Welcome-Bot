523bee82b9aa848f4c75a3c96bbbc6aa8da550ba
Fernando Oliveira <devphytols@gmail.com>
2021-05-03 10:13:43 -0300

    update

--- commands/setwelcome.js
+++ commands/setwelcome.js
@@ -2,24 +2,20 @@ const mongoose = require('mongoose')
 const welcomeChannelSchema = require('../models/welcome-channel')
 
 const cache = new Map()
-// An async function to load the data
+// Uma função Async para carregar os dados
 const loadData = async () => {
-  // Get all stored channel IDs
+  // Obtendo todos os IDs do canal armazenados
   const results = await welcomeChannelSchema.find({})
-  // Loop through them all and set them in our map
+  // Faz um loop
   for (const result of results) {
     cache.set(result._id, result.channelId)
   }
 }
-// Invoke this function when the bot starts up
 loadData()
 module.exports = {
   requiredPermissions: ['ADMINISTRATOR'],
   callback: async ({ message }) => {
-    // Destructure the guild and channel properties from the message object
     const { guild, channel } = message
-    // Use find one and update to either update or insert the
-    // data depending on if it exists already
     await welcomeChannelSchema.findOneAndUpdate(
       {
         _id: guild.id,
@@ -32,7 +28,7 @@ module.exports = {
         upsert: true,
       }
     )
-    // Store the information in the cache
+    // Armazena as informações no cache
     cache.set(guild.id, channel.id)
     message.reply('O canal de boas vindas foi configurado!')
   },
